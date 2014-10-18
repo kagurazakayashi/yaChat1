3b36def25db9769036c53338156e161f497e82cd
神楽坂雅詩 <cyscxchope@gmail.com>
2014-10-18 22:15:08 +0800

    完成从雅诗服务器读取雅诗的IP

---
+++ src/yachat/YaChat.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yachat;
+/**
+ *
+ * @author yashi
+ */
+public class YaChat {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("=== yaChat ===");
+         MainWindow mvc = new MainWindow();
+         mvc.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
